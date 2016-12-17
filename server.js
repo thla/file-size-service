ff2c7ae81bc55d66af315b108b184f7a1b548bb5
thla <thomas.lamparter@web.de>
2016-12-17 17:21:35 +0000

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express')
+var path =require('path');
+var multer  = require('multer')
+var upload = multer({ dest: './uploads/' })
+var fs = require("fs"); 
+
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.post('/get-file-size', upload.single('file'), function (req, res) {
+    var stats = fs.statSync(req.file.path);
+    res.send({ size: stats['size'] })
 })
 
 app.listen(parseInt(process.env.PORT), function () {
